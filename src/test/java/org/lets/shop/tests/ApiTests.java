5f9787233e730ba89f32077631a66233e47b8ac4
BunnySasi <sasikiran2213@gmail.com>
2025-08-30 18:03:08 +0530

    Updated 2.0

--- src/test/java/org/lets/shop/tests/ApiTests.java
+++ src/test/java/org/lets/shop/tests/ApiTests.java
@@ -1,9 +1,14 @@
 package org.lets.shop.tests;
 
 import io.restassured.*;
+import io.restassured.http.ContentType;
+
 import static io.restassured.RestAssured.*;
 import org.hamcrest.*;
 import static org.hamcrest.MatcherAssert.*;
+
+import java.io.File;
+
 import org.packs.ApiBase;
 import org.testng.annotations.Test;
 
@@ -14,5 +19,28 @@ public class ApiTests extends ApiBase {
 
 		given().spec(createRequestSpecification("https://rahulshettyacademy.com")).when().post("/api/ecom/auth/login")
 				.then().spec(createResponseSpecification());
+		System.out.println("Token:" + getToken());
+		System.out.println("Response:" + getResponses().jsonPath().getString("userId"));
+	}
+
+	/* Test AddProduct with Updated Code */
+
+	@Test
+	public void testName() throws Exception {
+		given().baseUri("https://rahulshettyacademy.com").contentType(ContentType.MULTIPART)
+				.header("authorization", getToken()).log().all().param("productName", "weeee")
+				.param("productAddedBy", getUserId())
+
+				.param("productCategory", "eee").param("productSubCategory", "Hiells").param("productPrice", "2222")
+				.param("productDescription", "wwww").param("productFor", "eerrr")
+				.multiPart("productImage", new File("pro3.jpg")).when().post("/api/ecom/product/add-product").then()
+				.spec(getDetailsProduct());
+
+	}
+
+	@Test
+	public void tests() throws Exception {
+		given().spec(createAddProduct(properties.getProperty("site"), "welecos", "eeee", "eeeeer", "55555", "eeee",
+				"wwwww", "pro3.jpg")).when().post("/api/ecom/product/add-product").then().spec(getDetailsProduct());
 	}
 }
--- src/test/java/org/packs/ApiBase.java
+++ src/test/java/org/packs/ApiBase.java
@@ -1,18 +1,29 @@
 package org.packs;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import io.restassured.*;
+import static io.restassured.RestAssured.*;
+import org.hamcrest.*;
+import static org.hamcrest.MatcherAssert.*;
 
+import io.restassured.builder.MultiPartSpecBuilder;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
+import io.restassured.internal.multipart.MultiPartSpecificationImpl;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.specification.MultiPartSpecification;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
@@ -24,13 +35,15 @@ public class ApiBase {
 	public static RequestSpecification requestSpecification;
 	public static ResponseSpecBuilder responseSpecBuilder;
 	public static ResponseSpecification responseSpecification;
-	public static Response response;
+	public static Response response, response2;
+	public static MultiPartSpecBuilder multiPartSpecBuilder;
+	public static MultiPartSpecification multiPartSpecification;
 
 	public static Properties getFileDetails(String fileName) {
 		properties = new Properties();
 		try {
-			fileInputStream = new FileInputStream(
-					System.getProperty("user.dir") + "\\src\\test\\java\\org\\lets\\shop\\tests\\" + fileName + ".properties");
+			fileInputStream = new FileInputStream(System.getProperty("user.dir")
+					+ "\\src\\test\\java\\org\\lets\\shop\\tests\\" + fileName + ".properties");
 			properties.load(fileInputStream);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -47,6 +60,21 @@ public class ApiBase {
 		requestSpecification = requestSpecBuilder.build();
 		createResponseSpecification();
 		return requestSpecification;
+	}
+
+	public static Response getResponses() {
+		Response myRes = given().spec(createRequestSpecification(properties.getProperty("site"))).when()
+				.post("/api/ecom/auth/login").then().spec(createResponseSpecification()).extract().response();
+		return myRes;
+	}
+
+	public static String getToken() {
+		String tokens = getResponses().jsonPath().getString("token");
+		return tokens;
+	}
+
+	public static String getUserId() {
+		return getResponses().jsonPath().getString("userId");
 	}
 
 	public static ResponseSpecification createResponseSpecification() {
@@ -69,4 +97,24 @@ public class ApiBase {
 		getFileDetails("data");
 	}
 
+	public static ResponseSpecification getDetailsProduct() {
+		responseSpecBuilder = new ResponseSpecBuilder();
+		responseSpecBuilder.expectStatusCode(201).expectContentType(ContentType.JSON).log(LogDetail.ALL);
+		responseSpecification = responseSpecBuilder.build();
+		return responseSpecification;
+	}
+
+	public static RequestSpecification createAddProduct(String url,String productName,String productCategory,String productSubCategory,
+			String productPrice,String productDescription,String productFor,String productImge) {
+		JsonPath jsonPath = new JsonPath(getResponses().asPrettyString());
+		requestSpecBuilder = new RequestSpecBuilder();
+		requestSpecBuilder.setBaseUri(url).setContentType(ContentType.MULTIPART)
+				.addHeader("Authorization", jsonPath.getString("token")).addParam("productName",productName)
+				.addParam("productAddedBy", jsonPath.getString("userId")).addParam("productCategory", productCategory)
+				.addParam("productSubCategory",productSubCategory).addParam("productPrice", productPrice)
+				.addParam("productDescription", productDescription).addParam("productFor", productFor)
+				.addMultiPart("productImage", new File(productImge));
+		requestSpecification = requestSpecBuilder.build();
+		return requestSpecification;
+	}
 }
